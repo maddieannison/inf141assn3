30eb9afcded4588becd47129c6192c18a8de9274
maddie annison <mannison@uci.edu>
2024-03-06 00:20:32 +0000

    added multithreading

--- src/partial_index.py
+++ src/partial_index.py
@@ -6,18 +6,23 @@ from collections import defaultdict
 class PartialIndex:
     def __init__(self):
         self.index = defaultdict(dict)
+        self.doc_id_mapping = {}
+        self.current_doc_id = 1
         
-    def index_document(self, document_id, text):
+    def index_document(self, filename, text):
+        doc_id = self.current_doc_id
+        self.doc_id_mapping[filename] = doc_id
         # Tokenize content        
         tokens = re.findall(r'\b\w+\b', text.lower())
         for token in tokens:
             if token in self.index:
-                if document_id in self.index[token]:
-                    self.index[token][document_id] += 1
+                if doc_id in self.index[token]:
+                    self.index[token][doc_id] += 1
                 else:
-                    self.index[token][document_id] = 1
+                    self.index[token][doc_id] = 1
             else:
-                self.index[token] = {document_id: 1}
+                self.index[token] = {doc_id: 1}
+        self.current_doc_id += 1
                 
     def offload(self, filename):
         with open(filename, 'w') as f:
